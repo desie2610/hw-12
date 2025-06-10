8925e4f073e2744e428f697449ddd4c4707ef141
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-06-10 16:31:46 +0300

    bvbnvbnvbnvbn

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// const user = {
+//   name: 'Oleh',
+//   age: 30,
+//   hobby: 'reading',
+//   premium: true
+// };
+
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+
+
+// for (let key of Object.keys(user)) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+
+// const countProps = obj => Object.keys(obj).length;
+
+// const userB = {
+//   name: 'Oleh',
+//   age: 30,
+//   hobby: 'skydiving',
+//   premium: false,
+// };
+
+// console.log(countProps(userB)); 
